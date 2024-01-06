0e26a5b87c1c929577b3df89d5116307e5cbe497
Egor Gerasimenko <gerasimenkoem@gmail.com>
2024-01-06 19:09:58 +0300

    Some adaptive

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ window.onload = () => {
     var headerMoving = function(direction){
         if (direction === "up"){
             $("header").removeClass("scrollDown");
-            $(".header__popup").removeClass("scrollDown");
+            $(".new-section").removeClass("scrollDown");
         } else if (direction === "down"){
             $("header").addClass("scrollDown");
-            $(".header__popup").addClass("scrollDown")
+            $(".new-section").addClass("scrollDown")
         }
     };
     var prevScrollTop = 0;
     document.addEventListener("scroll", function(){
-        if (window.screen.width < 930) {
+        if (window.screen.width < 930 && document.documentElement.scrollTop > 100) {
             var nextScrollTop = window.pageYOffset || 0; 
             if (nextScrollTop > prevScrollTop){
                 headerMoving("down");
@@ -26,11 +26,11 @@ window.onload = () => {
     $(".header__popup").on("click", function() {
         $(".header__popup i").eq(num % 2).toggleClass("rotateAnimation")
         $(".new-section").toggleClass("openPopup")
-        if (num % 2 == 0) {
-            $(".header__popup").transition({ y: '83px' });
-        } else {
-            $(".header__popup").transition({ y: '0px' });
-        }
+        // if (num % 2 == 0) {
+        //     $(".header__popup").transition({ y: '66px' });
+        // } else {
+        //     $(".header__popup").transition({ y: '66px' });
+        // }
         setTimeout(function () {
             $(".header__popup i").eq(num % 2).css('display', 'none')
             $(".header__popup i").eq((1 + num) % 2).css('display', 'inherit')
